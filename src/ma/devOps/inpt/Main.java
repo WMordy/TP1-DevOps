ffc36e89de72a7bf1e76c1d0ef646d51861785c1
Wadii <mwadii30@gmail.com>
2020-12-17 11:50:52 +0100

    Second edit for merge

--- src/ma/devOps/inpt/Main.java
+++ src/ma/devOps/inpt/Main.java
@@ -5,5 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("We are using Git and it's working !!!!");
 		System.out.println("Premiere modification");
+		System.out.print("let's merge the branch with main ");
 	}
 }
