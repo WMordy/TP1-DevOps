53f54ad3b1fed2fe88e7a5be18351479555c1d4d
Wadii <mwadii30@gmail.com>
2020-12-17 11:57:15 +0100

    ready to rebase

--- src/ma/devOps/inpt/Main.java
+++ src/ma/devOps/inpt/Main.java
@@ -3,7 +3,6 @@ package ma.devOps.inpt;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("We are using Git and it's working !!!!");
-		System.out.println("Premiere modification");
+	System.out.print("and we change every thing to rebase and nobody will have no clue!");
 	}
 }
